92309ebbe5d7dba51b0d1932d1caaa34e449906e
Julya Shapaeva <shapaeva03@mail.ru>
2024-08-06 05:15:24 +0400

    Add UserDetailView, UserUpdateView and UserDeleteView to urlpatterns

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from users.apps import UsersConfig
 from users.views import RegistrationView, email_confirm, LoginView, LogoutView, UserListView, user_blocking, \
-    user_unblocking
+    user_unblocking, UserDetailView, UserUpdateView, UserDeleteView
 
 app_name = UsersConfig.name
 
@@ -10,6 +10,10 @@ urlpatterns = [
     path('email-confirm/<str:token>/', email_confirm, name='email_confirm'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('<int:pk>/', UserDetailView.as_view(), name='user_detail'),
+    path('change/<int:pk>/', UserUpdateView.as_view(), name='user_update'),
+    path('delete/<int:pk>/', UserDeleteView.as_view(), name='user_delete'),
+
 
     path('', UserListView.as_view(), name='user_list'),
     path('user-block/<int:pk>/', user_blocking, name='user_block'),
